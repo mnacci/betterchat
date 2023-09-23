64c1efb7b340f6666146cde0245524ef5af4d490
mnacci <145075495+mnacci@users.noreply.github.com>
2023-09-23 13:44:36 +0800

    Update auth.ts

--- src/constants/auth.ts
+++ src/constants/auth.ts
@@ -2,6 +2,6 @@ export const officialAPIEndpoint = 'https://api.openai.com/v1/chat/completions';
 const customAPIEndpoint =
   import.meta.env.VITE_CUSTOM_API_ENDPOINT || '';
 export const defaultAPIEndpoint =
-  import.meta.env.VITE_DEFAULT_API_ENDPOINT || officialAPIEndpoint;
+  import.meta.env.VITE_DEFAULT_API_ENDPOINT || customAPIEndpoint;
 
 export const availableEndpoints = [officialAPIEndpoint, customAPIEndpoint];
