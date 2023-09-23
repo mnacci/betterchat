8f4cd542caf860489238fd45a590184c038eb551
mnacci <145075495+mnacci@users.noreply.github.com>
2023-09-23 13:37:02 +0800

    Update auth.ts

--- src/constants/auth.ts
+++ src/constants/auth.ts
@@ -1,6 +1,6 @@
-export const officialAPIEndpoint = 'https://api.renj.vip/v1/chat/completions';
+export const officialAPIEndpoint = 'https://api.openai.com/v1/chat/completions';
 const customAPIEndpoint =
-  import.meta.env.VITE_CUSTOM_API_ENDPOINT || 'https://api.openai.com/v1/chat/completions';
+  import.meta.env.VITE_CUSTOM_API_ENDPOINT || customAPIEndpoint;
 export const defaultAPIEndpoint =
   import.meta.env.VITE_DEFAULT_API_ENDPOINT || officialAPIEndpoint;
 
